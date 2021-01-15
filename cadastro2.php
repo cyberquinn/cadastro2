0c1b00b7d8fcb79ba7cf9b73940d48ecd2228bbc
PROG WEB - MANHA <PROG WEB - MANHA@MININT-633QJ9F>
2021-01-15 12:42:43 -0200

    Cadastro exemplo 2

---
+++ cadastro2.php
@@ -0,0 +1,180 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+
+        <link rel="stylesheet" href="plugins/Bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="plugins/icomoon/" />
+        <link rel="stylesheet" href="css/estilo.css" />    
+    </head>
+
+    <body>
+
+        <div class="container">
+            <h1> Novo cadastro </h1>
+
+            <a class="btn btn-outline-secondary btn-light">Pessoa física</a>
+            <a class="btn  btn-danger">Pessoa jurídica</a>
+            <form action="">
+                <div class="row">
+                    <div class="col">
+
+                        <div class="form-group">
+                            <h4>Dados da empresa</h4>
+                            <hr />
+                            <label for="exampleFormControlInput1">Razão social</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Razão social">
+                        </div>
+                        <h4 class="text-center">Dados Pessoais</h4>
+                        <hr />
+
+                        <!--coluna 5 itens-->
+                        <div class="row">
+
+                            <div class="col-lg">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <!--Fim da coluna 05 itens-->
+
+                        <!--coluna de 2 itens--> 
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--fim da coluna 2 itens-->
+
+                        <!--inicio da coluna 4-->
+                        <h4 class="text-center">Acesso ao Site </h4>
+                        <hr />
+                        <div class="row">
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                        </div>
+                        <!--fim da coluna 4-->
+                        <h4 class="text-center">Endereço </h4>
+                        <hr />
+                        <div class="row">
+                            <div class="col-8 col-sm">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                            <div class="col-4 col-sm">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                            <div class="col-sm">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm">
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+
+                                    <div class="form-group">
+                                        <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="exampleFormControlSelect1">Nome fantasia</label>
+                                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nome fantasia">
+                                    </div>
+                                    <hr />
+                                </div>
+                            </div>
+                            <!--ínicio linha-->
+                        </div>
+
+                    </div>
+
+                </div>  
+                <div class="form-group"> <button class="btn  btn-danger btn-block" type="submit">Cadastrar</button>   </div>
+            </form>
+        </div>
+    </body>
+</html>
